e28a5a5f3f6a54f5c20246f0f14b44d7efbcf6d7
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-02-05 23:50:55 +0100

    FIX room call

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,31 +1,17 @@
 import React from 'react';
 
 class Card extends React.Component {
-    state = {
-        turned: false,
-    };
-
-    componentDidMount() {
-        this.setState({ turned: this.props.turned });
-    }
-
-    handleClick = () => {
-        this.setState({ turned: true });
-    };
-
     render() {
-        const { turned } = this.state;
-        const { faction, word } = this.props;
-        const color = this.props.faction;
+        const { color, word, turned, onClick } = this.props;
 
         return (
             <div
                 className='card'
                 style={{
                     backgroundColor: turned ? color : 'white',
-                    color: turned && faction === 3 ? 'white' : 'black',
+                    color: turned && color === '#2d2d2d' ? 'white' : 'black',
                 }}
-                onClick={this.handleClick}
+                onClick={() => onClick(word)}
             >
                 {word}
             </div>
--- src/components/cardMatrix.jsx
+++ src/components/cardMatrix.jsx
@@ -6,9 +6,7 @@ class CardMatrix extends Component {
     static contextType = SocketContext;
 
     state = {
-        words: '',
-        factions: '',
-        leader: true,
+        gameData: '',
     };
 
     componentDidMount() {
@@ -16,26 +14,45 @@ class CardMatrix extends Component {
 
         socket.emit('join', this.props.room);
 
-        this.context.emit('data', 'data', async data => {
-            const { words, factions } = await data;
-            this.setState({ words, factions });
+        this.context.emit('data', 'data', async currentGameData => {
+            const gameData = await currentGameData.data;
+            this.setState({ gameData });
+        });
+
+        socket.on('turn', currentGameData => {
+            const gameData = currentGameData;
+            console.log(gameData);
+            console.log('Got turned data from Server');
+            this.setState({ gameData });
         });
     }
 
+    handleClick = word => {
+        const socket = this.context;
+        const gameData = [...this.state.gameData];
+        gameData.forEach(row => {
+            const target = row.find(entry => entry.word === word);
+            if (target) target.turned = true;
+        });
+        socket.emit('turn', gameData);
+    };
+
     render() {
-        const { words, factions, leader } = this.state;
+        const { gameData } = this.state;
 
         return (
             <main className='card-container'>
-                {factions &&
-                    factions.map((row, i) => {
-                        return row.map((faction, j) => {
+                {gameData &&
+                    gameData.map((row, i) => {
+                        return row.map((entry, j) => {
+                            const { color, word, turned } = entry;
                             return (
                                 <Card
                                     key={`${i}-${j}`}
-                                    faction={faction ? faction : 0}
-                                    word={words ? words[i][j] : '...'}
-                                    turned={leader}
+                                    color={color}
+                                    word={word}
+                                    turned={turned}
+                                    onClick={this.handleClick}
                                 />
                             );
                         });
