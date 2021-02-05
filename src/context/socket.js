e0429530b887e1ce846e7b4d32a902eab32455d6
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-02-05 21:28:43 +0100

    ADD server address

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client";
 import React from 'react';
-const SOCKET_URL = "http://localhost:3001"
+const SOCKET_URL = "https://open-code-names.herokuapp.com"
 
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = React.createContext();
